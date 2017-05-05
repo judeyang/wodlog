bb156d798c2b8cd68b17e566b06433ea16cd92d3
JudeYang <byoox@qq.com>
2017-05-05 23:23:00 +0800

    add name in user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503155341) do
+ActiveRecord::Schema.define(version: 20170505133916) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170503155341) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_owner",               default: false
+    t.string   "name"
+    t.string   "wechat"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
